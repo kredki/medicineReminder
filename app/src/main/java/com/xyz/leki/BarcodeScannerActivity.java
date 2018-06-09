58e1542402b9cdf46d8cec29e821e851df625071
TL <31960565+kredki@users.noreply.github.com>
2018-06-09 16:04:00 +0200

    save quantity after barcode reading added

--- app/src/main/java/com/xyz/leki/BarcodeScannerActivity.java
+++ app/src/main/java/com/xyz/leki/BarcodeScannerActivity.java
@@ -2,28 +2,48 @@ package com.xyz.leki;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.drawable.BitmapDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.util.SparseArray;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.NumberPicker;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.android.gms.vision.Frame;
 import com.google.android.gms.vision.barcode.Barcode;
 import com.google.android.gms.vision.barcode.BarcodeDetector;
+import com.xyz.leki.Resource.Medicine;
+import com.xyz.leki.Resource.MedicineList;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.List;
 
 public class BarcodeScannerActivity extends AppCompatActivity {
     private Bitmap myBitmap;
+    private List<Medicine> meds;
+    private Button saveButton;
+    private NumberPicker medQuantityNumberPicker;
+    TextView medNameTextView;
+    int medIndex;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_barcode_scanner);
+        medIndex = -1;
+        saveButton = findViewById(R.id.saveQuanitytyButton);
+        medQuantityNumberPicker = findViewById(R.id.medQuantityNumberPicker);
+        medNameTextView = findViewById(R.id.medNameTextView);
+        saveButton.setVisibility(View.GONE);
+        medQuantityNumberPicker.setVisibility(View.GONE);
+        medNameTextView.setVisibility(View.GONE);
+
         Button btn = (Button) findViewById(R.id.scanButton);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -61,11 +81,68 @@ public class BarcodeScannerActivity extends AppCompatActivity {
         if (barcodes.size() != 0) {
             Barcode thisCode = barcodes.valueAt(0);
             txtView.setText(thisCode.rawValue);
+            int i = findMedIndex(thisCode.rawValue);
+            if(i > -1) {
+                medIndex = i;
+                setMedInfo(i);
+            } else
+                txtView.setText("Nie znaleziono leku na liście");
         } else {
             txtView.setText("Nie znaleziono kodu");
         }
     }
 
+    //find med id returns -1 if not found
+    private int findMedIndex(String barcode) {
+        meds = MedicineList.getMedicineList();
+        int i = 0;
+        for (Medicine med : meds) {
+            if(med.getBarcode().equals(barcode))
+                return i;
+            i++;
+        }
+        return -1;
+    }
+
+    private void setMedInfo(int index) {
+        saveButton.setVisibility(View.VISIBLE);
+        medQuantityNumberPicker.setVisibility(View.VISIBLE);
+        medNameTextView.setVisibility(View.VISIBLE);
+
+        Medicine med = meds.get(index);
+        medNameTextView.setText(med.getName());
+        medQuantityNumberPicker.setMinValue(0);
+        medQuantityNumberPicker.setMaxValue(1000);
+        medQuantityNumberPicker.setValue(med.getQuantity());
+    }
+
+    public void saveMed(View view) {
+        if (medIndex > -1) {
+            Medicine med = meds.get(medIndex);
+            med.setQuantity(medQuantityNumberPicker.getValue());
+            meds.set(medIndex, med);
+            writeArray(meds);
+        }
+    }
+
+    public void writeArray(List<Medicine> medicineList) {
+        File f = new File(getFilesDir(), "medicine_list.srl");
+        try {
+            FileOutputStream fos = new FileOutputStream(f);
+            ObjectOutputStream objectwrite = new ObjectOutputStream(fos);
+            objectwrite.writeObject(medicineList);
+            fos.close();
+
+            if (!f.exists()) {
+                f.mkdirs();
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 //    public void scanBarcode(View view) {
 //        ImageView barcodeImageView = findViewById(R.id.barcodeImageView);
 //        Bitmap bitmap=((BitmapDrawable)barcodeImageView.getDrawable()).getBitmap();
